35aae813b18bbf82d60b94d06c510bc1c6ff5f80
Luke Fiorio <lukefiorio@Lukes-MacBook-Pro.local>
2019-01-29 20:59:17 -1000

    q1

--- js/app.js
+++ js/app.js
@@ -4,6 +4,20 @@
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
 
 
+var naviClass = document.getElementsByClassName('navi');
+
+for (var i=0; i<naviClass.length; i++) {
+    naviClass[i].addEventListener('click', NavDropDown);
+}
+// is there a way to store the display status as a variable?
+function NavDropDown() {
+    if (this.querySelectorAll('.inner')[0].style.display==='none') {
+        this.querySelectorAll('.inner')[0].style.display='block';
+    } else {
+        this.querySelectorAll('.inner')[0].style.display='none';
+    }
+}
+
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
 
