1cd474a7e375935d067a3c4c63a325e9af3b6acc
Luke Fiorio <lukefiorio@Lukes-MacBook-Pro.local>
2019-01-29 21:57:58 -1000

    q2 & half q3

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ for (var i=0; i<naviClass.length; i++) {
 }
 // is there a way to store the display status as a variable?
 function NavDropDown() {
-    if (this.querySelectorAll('.inner')[0].style.display==='none') {
+    if (this.querySelectorAll('.inner')[0].style.display==='none' || this.querySelectorAll('.inner')[0].style.display==='') {
         this.querySelectorAll('.inner')[0].style.display='block';
     } else {
         this.querySelectorAll('.inner')[0].style.display='none';
@@ -21,7 +21,40 @@ function NavDropDown() {
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
 
+var nameClass = document.getElementsByClassName('name');
+
+for (var i = 0; i<nameClass.length; i++) {
+    nameClass[i].addEventListener('click',feedMe);
+}
+
+function feedMe() {
+    if (this.querySelectorAll('.menu')[0].style.display==='block') {
+        this.querySelectorAll('.menu')[0].style.display='none';
+    } else {
+        this.querySelectorAll('.menu')[0].style.display='block';
+    }
+}
+
+
 /*3) to the thumbs down icon that will add a count (counter) for each time the icon is clicked on.*/
 
 
 
+
+//var downThumbClass = document.getElementsByClassName('svg-inline--fa fa-thumbs-down fa-w-16');
+// console.log(downThumbClass);
+// console.log(downThumbClass.length);
+
+//var downThumbClass = document.getElementsByClassName('far fa-thumbs-down');
+
+// dont like that it clicks the button no matter where along the horizontal i click.
+var downThumbClass = document.getElementsByClassName('thumb');
+console.log(downThumbClass.length);
+
+for (var i=0;i<downThumbClass.length; i++) {
+    downThumbClass[i].addEventListener('click',downCnt);
+}
+
+function downCnt() {
+    console.log('boo!!');
+}
